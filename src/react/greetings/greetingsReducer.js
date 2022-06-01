5451720a2395f37fc89b3ba1cd3fe30720ea50fa
Atsighi Bright <brytebee@gmail.com>
2022-06-01 19:57:33 +0100

    :white_check_mark: pdate greetingsReducer
    Add getDataFromAPI function

--- src/react/greetings/greetingsReducer.js
+++ src/react/greetings/greetingsReducer.js
@@ -1,6 +1,14 @@
 const GET_DATA = "src/redux/GET_DATA";
+const URL = "http://localhost:3000/api/v1/greetings";
 
-const getData = (payload) => ({
+export const getData = (payload) => ({
   type: GET_DATA,
   payload,
 });
+
+export const getDataFromAPI = async () => (dispatch) => {
+  const req = await fetch(URL);
+  const res = await req.json();
+  const data = res.data;
+  dispatch(getData(data))
+};
