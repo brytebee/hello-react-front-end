9577a8c27df4b4ee2980e1fd9412f53f1dec49cb
Atsighi Bright <brytebee@gmail.com>
2022-06-02 02:44:28 +0100

    :white_check_mark: Update Greetings component
    useSelector to get store Data

--- src/component/Greetings.jsx
+++ src/component/Greetings.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Greetings = () => {
+  const data = useSelector((store) => store.greetRed);
+  console.log(data);
   return (
     <div>
       <h1>Greetings</h1>
