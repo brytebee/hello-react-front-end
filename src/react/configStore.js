a60a1a590cc7b870b84bf44cfd40af9edf478ce4
Atsighi Bright <brytebee@gmail.com>
2022-06-02 02:47:48 +0100

    :fire: Update configStore
    Reflect name change from greetReducer

--- src/react/configStore.js
+++ src/react/configStore.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import reducer from "./greetings/greetingsReducer";
+import greetReducer from "./greetings/greetingsReducer";
 
 const reducer = combineReducers({
-  greetRed: reducer,
+  greetRed: greetReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
