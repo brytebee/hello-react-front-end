9836332c6ca105df50ade6f7a92b71afc44019c5
Atsighi Bright <brytebee@gmail.com>
2022-06-02 04:39:45 +0100

    :white_check_mark: Update Greetings.jsx
    Implement random display

--- src/component/Greetings.jsx
+++ src/component/Greetings.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDataFromAPI } from "../react/greetings/greetingsReducer";
 
 const Greetings = () => {
   const allGreetings = useSelector((state) => state.greetRed);
   const dispatch = useDispatch();
+  const [message, setMessage] = useState([
+    {
+      id: 5,
+      title: "English",
+      message: "Good day",
+    },
+  ]);
 
-  const handleRand = () => {};
+  const handleRand = () => {
+    const newMessage = allGreetings[~~(allGreetings.length * Math.random())];
+    setMessage([newMessage]);
+  };
 
   useEffect(() => {
     dispatch(getDataFromAPI());
@@ -15,15 +25,15 @@ const Greetings = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      {/* {allGreetings.map((greeting) => {
+      {message.map((greeting) => {
         return (
           <div key={greeting.id}>
             <h4>{greeting.title}</h4>
             <p>{greeting.message}</p>
           </div>
         );
-      })} */}
-      <button type="button" onClick="handleRand">
+      })}
+      <button type="button" onClick={handleRand}>
         Say Hi
       </button>
     </div>
