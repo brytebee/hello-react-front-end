6d04405c4930f921d172bdb6cec9096fc16457a3
Atsighi Bright <brytebee@gmail.com>
2022-06-02 02:33:56 +0100

    :white_check_mark: Update configStore
    Export store

--- src/react/configStore.js
+++ src/react/configStore.js
@@ -1,8 +1,12 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import reducer from "./greetings/greetingsReducer";
 
 const reducer = combineReducers({
   greetRed: reducer,
 });
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+export default store;
